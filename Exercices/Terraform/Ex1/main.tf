7de1054ae06812564488735c084026ef9643c7e4
Eric PLANCHE <eric.planche@dekra.com>
2026-04-27 10:15:14 +0200

    add .gitignore & clean EX1

--- Exercices/Terraform/Ex1/main.tf
+++ Exercices/Terraform/Ex1/main.tf
@@ -11,8 +11,8 @@ terraform {
 
   # Configuration du stockage distant pour le fichier d'état
   backend "azurerm" {
-    resource_group_name  = "RG-TERRAFORM-BACKEND"
-    storage_account_name = "sttfstatelab"
+    resource_group_name  = "RG-B3-Eric"
+    storage_account_name = "sttfstatelabynovepe"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
@@ -22,31 +22,41 @@ provider "azurerm" {
   features {}
 }
 
+variable "vm_admin_password" {
+  description = "Admin password for the Linux VM"
+  type        = string
+  sensitive   = true
+}
+
 # -----------------------------------------------------------------------
 # 2. CRÉATION DES RESSOURCES
 # -----------------------------------------------------------------------
 
 # Création du Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "RG-LAB-TERRAFORM"
-  location = "France Central"
-  tags = {
-    environment = "lab"
-  }
+# resource "azurerm_resource_group" "rg" {
+#   name     = "RG-LAB-TERRAFORM"
+#   location = "France Central"
+#   tags = {
+#     environment = "lab"
+#   }
+# }
+
+data "azurerm_resource_group" "rg" {
+  name = "RG-B3-Eric"
 }
 
 # Réseau Virtuel (VNET)
 resource "azurerm_virtual_network" "vnet" {
   name                = "VNET-TF"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 # Sous-réseau (Subnet)
 resource "azurerm_subnet" "subnet" {
   name                 = "SUBNET-APP"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
@@ -54,8 +64,8 @@ resource "azurerm_subnet" "subnet" {
 # Adresse IP Publique
 resource "azurerm_public_ip" "pip" {
   name                = "IP-PUBLIC-VM"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -63,8 +73,8 @@ resource "azurerm_public_ip" "pip" {
 # Interface Réseau (NIC) - Le câble de la VM
 resource "azurerm_network_interface" "nic" {
   name                = "NIC-VM-TF"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "internal"
@@ -77,21 +87,16 @@ resource "azurerm_network_interface" "nic" {
 # Machine Virtuelle Linux
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "VM-TERRAFORM-01"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   size                = "Standard_B1s"
   admin_username      = "azureuser"
+  admin_password      = var.vm_admin_password
   disable_password_authentication = false
 
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
-
-  # Utilisation d'une clé SSH générée localement (nécessite ~/.ssh/id_rsa.pub)
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
 
   os_disk {
     caching              = "ReadWrite"
